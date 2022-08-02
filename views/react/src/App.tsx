31342e19300efed27d3b13d1fabafc46923395f1
Nick <nickas951@gmail.com>
2022-08-02 19:51:26 +0300

    first commit

---
+++ views/react/src/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+    const [count, setCount] = useState(0);
+
+    return <div className="App"></div>;
+}
+
+export default App;
