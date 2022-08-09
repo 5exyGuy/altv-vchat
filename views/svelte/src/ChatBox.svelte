fea178f86a3a3d6b5d569971f172b3303f3c9ecd
Nick <nickas951@gmail.com>
2022-08-09 19:29:33 +0300

    Update

--- views/svelte/src/ChatBox.svelte
+++ views/svelte/src/ChatBox.svelte
@@ -1,83 +1,25 @@
 <script lang="ts">
     import { onMount, tick } from 'svelte';
-    import Button from './components/Button.svelte';
-    import Channel from './components/Channel.svelte';
-    import Message from './components/Message.svelte';
     import MessageInput from './components/MessageInput.svelte';
-    import ChevronDownIcon from './icons/ChevronDownIcon.svelte';
-    import PlusIcon from './icons/PlusIcon.svelte';
+    import Messages from './components/Messages.svelte';
 
-    const channels = new Map<string, { closable: boolean }>([
-        ['global', { closable: false }],
-        ['local', { closable: true }],
-    ]);
-    const channelMessages = new Map<string, Array<string>>([
-        ['global', []],
-        ['local', ['Hello', 'World']],
-    ]);
-
-    let messagesRef: HTMLDivElement;
-    let currentChannel = 'global';
-    let messages = channelMessages.get(currentChannel);
+    let messages = [];
 
     async function processMessageSend(event: CustomEvent<{ message: string }>) {
         const { message } = event.detail;
         if (!message || typeof message !== 'string' || message.length === 0) return;
 
-        const channel = channelMessages.get(currentChannel);
-        if (!channel) return;
-
-        messages = [...channel, message];
-        channelMessages.set(currentChannel, messages);
-
-        await tick();
-        messagesRef?.scrollTo({ behavior: 'smooth', top: messagesRef.scrollHeight });
-    }
-
-    function processMessageScroll(event: Event) {}
-
-    async function processChannelClick(event: CustomEvent<{ name: string }>) {
-        currentChannel = event.detail.name;
-        messages = channelMessages.get(currentChannel);
-
-        await tick();
-        messagesRef?.scrollTo({ top: messagesRef.scrollHeight });
+        messages = [...messages, message];
     }
 
     onMount(() => {
-        messagesRef?.scrollTo({ behavior: 'smooth', top: messagesRef.scrollHeight });
-
         if (window.alt) {
             window.alt.emit('vchat:mounted');
         }
     });
 </script>
 
-<div class="chat-box">
-    <div class="header">
-        <div class="channels">
-            {#each [...channels] as channel}
-                <Channel name={channel[0]} closable={channel[1].closable} on:click={processChannelClick} />
-            {/each}
-        </div>
-        <!-- <div class="controls">
-            <Button shape="square"><ChevronDownIcon /></Button>
-            <Button shape="square"><PlusIcon /></Button>
-        </div> -->
-    </div>
-    {#if messages.length > 0}
-        <div class="messages" bind:this={messagesRef} on:scroll={processMessageScroll}>
-            {#each messages as message}
-                <Message>{message}</Message>
-            {/each}
-            <!-- <button class="absolute right-0">
-            <ChevronDownIcon />
-        </button> -->
-        </div>
-    {/if}
+<div class="flex flex-col gap-4 p-2 top-0 w-96">
+    <Messages {messages} />
     <MessageInput on:send={processMessageSend} />
 </div>
-
-<style lang="scss">
-    @import 'ChatBox.scss';
-</style>
---
+++ views/svelte/src/components/Messages.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import Message from './Message.svelte';
+
+    export let messages: Array<string> = [];
+
+    let messagesRef: HTMLDivElement;
+</script>
+
+{#if messages.length > 0}
+    <div class="flex flex-col gap-2 max-h-40 overflow-y-scroll" bind:this={messagesRef}>
+        {#each messages as message}
+            <Message>{message}</Message>
+        {/each}
+    </div>
+{/if}
+
+<style lang="scss">
+    ::-webkit-scrollbar {
+        background: transparent;
+        width: 0;
+    }
+</style>
