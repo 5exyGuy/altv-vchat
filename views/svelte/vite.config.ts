41f50524c6c873c8874e58c4b126e4c75baa6a24
Nick <nickas951@gmail.com>
2022-08-22 13:34:12 +0300

    Fix vite base path

--- views/svelte/vite.config.ts
+++ views/svelte/vite.config.ts
@@ -4,5 +4,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [svelte()],
+    base: './',
     build: { outDir: '../../client/svelte-view' },
 });
